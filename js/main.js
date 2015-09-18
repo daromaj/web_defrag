c879209f1a11ba7b80bcb954e24e1bf9d359074a
daromaj <dariuszmajewski@gmail.com>
2015-09-18 00:13:12 -0400

    independent cubes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var cubes = [];
+var types = ["good", "bad", "system", "empty"];
+function markAsGood(c) {
+    var el = $("#" + c.id);
+    el.removeClass("bad");
+    el.addClass("good");
+    c.type = 0;
+}
 $(function () {
     var mainDiv = $('#main');
     //7px cubes
@@ -6,17 +14,44 @@ $(function () {
     var parts = Math.floor(screenWidth / 7) * Math.floor(screenHeight / 7);
     var sectorCounter = 0;
     var i = 0;
-    var types = ["good", "bad", "system", "empty"];
+    var badCubes = 0;
+    var systemCubes = 0;
+    var emptyCubes = 0;
     while (i < parts) {
-        $(mainDiv).append("<span id='sector" + sectorCounter + "'></span");
-        var sector = $("#sector" + sectorCounter);
-        var classType = Math.floor(Math.random()*4);
-        sector.addClass(types[classType]);
+//        $(mainDiv).append("<span id='sector" + sectorCounter + "'></span");
+//        var sector = $("#sector" + sectorCounter);
+        var classType = Math.floor(Math.random() * 3) + 1;
+//        sector.addClass(types[classType]);
         var sectorSize = ((parts - i) > 10) ? Math.floor((Math.random() * 10) + 1) : (parts - i);
         for (var j = 0; j < sectorSize; j++) {
-            sector.append("<span id='part" + (i + j) + "' class='cube'></span>");
+            var id = "part" + (i + j);
+            mainDiv.append("<span id='" + id + "' class='cube " + types[classType] + "'></span>");
+            switch (classType) {
+                case 1:
+                    badCubes++;
+                    break;
+                case 2:
+                    systemCubes++;
+                    break;
+            }
+            cubes.push({
+                id: id,
+                type: classType,
+                index: (i + j)
+            });
         }
         i += sectorSize;
         sectorCounter++;
     }
-})
+    //some of the bad cubes can be marked as good
+    for (var i = 0; i < badCubes; i++) {
+        if (cubes[i].type === 1) {
+            setTimeout(function () {
+                markAsGood(cubes[i]);
+            }, 100 * i);
+        }
+    }
+    //screen ready
+    //simple find and replace
+});
+
